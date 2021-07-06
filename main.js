34fc23afeb14e6cd361c92fa24560436d096b017
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-07-06 21:38:41 +0200

    Almost all HTML, part CSS, starting main.js .2

---
+++ main.js
@@ -0,0 +1,9 @@
+// canvas
+let canvas = document.querySelector("#my-canvas")
+let ctx = canvas.getContext("2d")
+
+let splashScreen = document.querySelector("#splash-screen")
+let startButton = document.querySelector("#start-btn")
+
+
+// Event listeners
